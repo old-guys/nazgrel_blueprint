ec73f62dd9ee963dcf5e1210ef6b600a24292caf
francis jiang <jhjguxin@gmail.com>
2018-01-02 10:17:48 +0800

    [deploy] Reset pm2 process via pm2_start deploy task

--- shipitfile.js
+++ shipitfile.js
@@ -22,10 +22,15 @@ module.exports = function (shipit) {
   });
   shipit.task('pm2_start', function () {
     // return shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && nohup npm run start_channel > log/start_channel.log 2>&1 &');
-    return shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm --name channel_mock -- run start_channel');
-    return shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm  --name web_mock -- run start_web');
-    return shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm --name dev_mock -- run start_dev');
-    return shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm --name open_mobile_mock -- run start_open_mobile');
+    // stop and delete from pm2 process lis
+    shipit.remote("pm2 delete channel_mock web_mock dev_mock open_mobile_mock");
+    // start and daemonize
+    shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm --name channel_mock -- run start_channel');
+    shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm  --name web_mock -- run start_web');
+    shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm --name dev_mock -- run start_dev');
+    shipit.remote('cd /ishanggang/apps/nazgrel_blueprint/current && pm2 start npm --name open_mobile_mock -- run start_open_mobile');
+
+    return null;
   });
   shipit.task('ps', function () {
     // return shipit.remote("ps aux | grep drakov | grep -v 'grep'");
